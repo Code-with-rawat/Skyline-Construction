c07975965b40dc44161e04cd082eb9be71c19f42
Code-with-rawat <rawathimanshu2005r@gmail.com>
2025-10-30 09:51:47 +0530

    jaiganeshhjiii

--- client/src/Components/Common/Https.jsx
+++ client/src/Components/Common/Https.jsx
@@ -1,5 +1,5 @@
 export const apiUrl = 'https://skyline-construction-6.onrender.com/api/';
-export const fileUrl = 'https://skyline-construction-6.onrender.com/';
+export const fileUrl = 'https://skyline-construction-6.onrender.com';
 export const token = () => {
       const userinfo = localStorage.getItem('userinfo');
       const data = JSON.parse(userinfo);
